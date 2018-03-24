a3536a544b034bdf2ac14e959473a8e041a3d23c
Fela Maslen <felamaslen@gmail.com>
2018-03-24 22:07:55 +0000

    Added index for exporting modules

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import LineGraphComponent from './LineGraph';
+import TimeGraphComponent from './TimeGraph';
+
+export const LineGraph = LineGraphComponent;
+export const TimeGraph = TimeGraphComponent;
+
